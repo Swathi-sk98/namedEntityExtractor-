fc67dd623b04e50152d85624cc3363b50681e465
Swathi-sk98 <56061604+Swathi-sk98@users.noreply.github.com>
2019-11-26 21:55:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-import nltk
+import spacy
 import os
 from flask import Flask,request,jsonify,render_template
 from flask_pymongo import PyMongo
 from pymongo import MongoClient
 from celery import Celery
-from nltk.tokenize import sent_tokenize,word_tokenize
-
+from spacy import displacy
+from collections import Counter
+import en_core_web_sm
+nlp = spacy.load('en_core_web_sm')
 
 app = Flask(__name__)
 
@@ -16,7 +18,7 @@ app.config["MONGO_URI"] = "mongodb://localhost:27017/Entities"
 
 #Celery configuration
 app.config['CELERY_BROKER_URL'] = 'redis://localhost:6379'
-app.config['CELERY_RESULT_BACKEND'] = 'redis://localhost:6379'
+app.config['RESULT_BACKEND'] = 'redis://localhost:6379'
 
 #Initialize extensions
 mongo = PyMongo(app)
@@ -25,50 +27,51 @@ mongo = PyMongo(app)
 celery = Celery(app.name, broker = app.config['CELERY_BROKER_URL'])
 celery.conf.update(app.config)
 
-    
+
 @celery.task(bind=True)
-def extract_entity(self,input_text):
+def extract_entity(self,input_text,flag):
     """Background task to extract named entities."""
     self.input_text = input_text
-    tokenized = nltk.sent_tokenize(input_text)
-    for i in tokenized:
-        words = nltk.word_tokenize(i)
-        tagged = nltk.pos_tag(words)
-        n = []
+    self.flag = flag
+    if flag == False:
+        doc = nlp(input_text)
+        ent = []
 
-        named_ent = nltk.ne_chunk(tagged)
-
-        for chunk in named_ent:
-            if hasattr(chunk, 'label'):
-                chunk = chunk[0]
-            (name,tag) = chunk
-            if tag == 'NN':
-                n.append(name)
-
+        for x in doc.ents:
+            ent.append(x.text)
+        
         dictionary = [{
             'text':input_text,
-            'entities':n
+            'entities':ent
             }]
              
         namedEntities = mongo.db.namedEntities
-        dictionary_add = {'name':dictionary}
-        namedEntities.insert(dictionary_add)
         
-        return named_ent
+        namedEntities.update(
+            {'text' : input_text},
+            dictionary,
+            True
+            )
+              
+        flag = True
+        
+    return doc
 
 
 @app.route('/')
 def index():
     return render_template("index.html")
 
-@app.route('/process_content',methods = ["POST"])
-def process_content():
+@app.route('/store_content',methods = ["POST"])
+def store_content():
     if request.method == 'POST':
         rawtext = request.form['rawtext']
-        task = extract_entity.delay(rawtext)                           
+        processed = False
+        task = extract_entity.delay(rawtext,processed)                           
                            
         return render_template("index.html", celery = task )
                                                                                      
-
 if __name__ == '__main__':
     app.run(debug=True)
+                                                                                         
+
