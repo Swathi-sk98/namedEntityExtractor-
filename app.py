effa1994ee7eb9208cc331ea78aa5fc9e2dd244a
Swathi-sk98 <56061604+Swathi-sk98@users.noreply.github.com>
2020-01-26 17:35:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,28 @@
 import spacy
+from spacy.vocab import Vocab
 import os
 from flask import Flask,request,jsonify,render_template
 from flask_pymongo import PyMongo
 from pymongo import MongoClient
-from celery import Celery
+from celery import Celery, current_app
+from celery.bin import worker
 from spacy import displacy
 from collections import Counter
 import en_core_web_sm
 nlp = spacy.load('en_core_web_sm')
 
+#to add new entity types
+custom_entities = ['Bootstrap','Oracle','Python','MySQL','Django']
+p = nlp.get_pipe('ner')
+for ent in custom_entities:
+    if 'extra_labels' in p.cfg and ent in p.cfg['extra_labels']:
+        pass
+    else:
+        p.add_label(ent)
+
+p.cfg['extra_labels'] = custom_entities
+
+#vocab = Vocab(strings=['bootstrap','oracle','python','mysql','django'])
 app = Flask(__name__)
 
 #mongoDB configuration
@@ -26,14 +40,13 @@ mongo = PyMongo(app)
 celery = Celery(app.name, broker = app.config['CELERY_BROKER_URL'])
 celery.conf.update(app.config)
 
-
 @celery.task(bind=True)
 def extract_entity(self,input_text,flag):
     """Background task to extract named entities."""
     self.input_text = input_text
     self.flag = flag
     if flag == False:
-        doc = nlp(input_text) #extracts entities
+        doc = nlp(input_text)
         ent = []
 
         for x in doc.ents:
@@ -43,33 +56,37 @@ def extract_entity(self,input_text,flag):
             'text':input_text,
             'entities':ent
             }]
-
-        #updates to mongoDB     
+             
         namedEntities = mongo.db.namedEntities
-        dictionary_add = {'name': dictionary}
+        dictionary_add = {'name':dictionary}
         if namedEntities.find({'name':dictionary}).count()==0:
-            namedEntities.insert(dictionary) 
-    
-        flag = True
+            namedEntities.insert(dictionary_add)
         
-    return doc
+        flag = True
+       
+    for x in doc.ents:
+        print(x.text)
+        return x.text
 
 
-@app.route('/') #to check whether the application is running correctly on port
+@app.route('/')
 def index():
     return render_template("index.html")
+
+
 
 @app.route('/store_content',methods = ["POST"])
 def store_content():
     if request.method == 'POST':
         rawtext = request.form['rawtext']
         processed = False
-        task = extract_entity.delay(rawtext,processed)   
+        task = extract_entity.delay(rawtext,processed)
+        print(task)   
 
                
         return render_template("index.html",celery = task)
 
-@app.route('/view_entities', methods = ['GET'])  #viewing all contents of namedEntities
+@app.route('/view_entities', methods = ['GET'])
 def get_entities():
     namedEntities = mongo.db.namedEntities
     Lists = []
